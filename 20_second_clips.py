b182ad0e2911ff03c7139bde02535816700ba851
abstract-enough <77987885+abstract-enough@users.noreply.github.com>
2026-06-02 21:22:08 -0400

    Create 20_second_clips.py

---
+++ 20_second_clips.py
@@ -0,0 +1,31 @@
+import cv2
+import imageio
+
+file_name = "cardinal.mp4"
+
+cap = cv2.VideoCapture("/content/drive/MyDrive/"+file_name)
+
+frame_rate = cap.get(5)
+
+frames = []
+
+i=1
+j=1
+while True:
+
+    ret, frame = cap.read()
+    if ret:
+      color_frame = cv2.cvtColor(frame,
+cv2.COLOR_BGR2RGB)
+      frames.append(color_frame)
+
+      if i%(20*frame_rate) == 0:
+        #write file
+        imageio.mimsave("/content/drive/MyDrive/"+file_name[:-4]+str(j)+".mp4", frames, fps=frame_rate)
+        frames = []
+        j=j+1
+
+      i=i+1
+
+    if not ret:
+        break
